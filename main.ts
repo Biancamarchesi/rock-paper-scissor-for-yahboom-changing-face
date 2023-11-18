9c503d4731f67148f10c747bc65f7c83092168a6
Biancamarchesi <116459207+Biancamarchesi@users.noreply.github.com>
2023-11-18 16:39:21 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,56 @@
+let mão = 0
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    basic.clearScreen()
+    basic.pause(2000)
+    mão = randint(1, 3)
+    if (mão == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (mão == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (mão == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+})
